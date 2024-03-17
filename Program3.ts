4b14ac2b6699e0157193ed361a3ad36d6a0f8d84
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-17 19:11:44 +0530

    Add files via upload

---
+++ Program3.ts
@@ -0,0 +1,24 @@
+let Name = "Siddhesh"
+let Score = 32
+
+console.log("My name is:" + Name + " " + "and my score is : " + Score)
+
+console.log(`My name is: ${Name} and my score is : ${Score}`)
+
+let networkData : any ="this is data"
+console.log(networkData)
+console.log(typeof networkData)
+
+
+let serverData : any = {
+    productname:'Samsung',
+    price:32500
+}
+console.log(serverData)
+console.log(typeof serverData)
+
+console.log(serverData.productname)
+console.log(serverData.price)
+
+
+
