be33efdc8126298ca401393b26e5e04aa13283fd
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-17 19:10:56 +0530

    Add files via upload

---
+++ Program2.ts
@@ -0,0 +1,22 @@
+let x : number = 12
+console.log(x)
+console.log(typeof x)
+
+let str : string = "Hello Duniya"
+console.log(str)
+console.log(typeof str)
+
+
+let bool : boolean = true
+console.log(bool)
+console.log(typeof bool)
+
+let value = null
+console.log(value)
+console.log(typeof value)
+
+let notdef;
+console.log(notdef)
+console.log(typeof notdef)
+
+ 
