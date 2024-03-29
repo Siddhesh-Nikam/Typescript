b544ea8a2bed0bb5df9f007c326370fdd5689f99
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-29 15:16:34 +0530

    Add files via upload

---
+++ Program6.ts
@@ -0,0 +1,17 @@
+//Objects
+
+let student : {
+    name:string;
+    address:string;
+    phone:number;
+    isActive:boolean;
+} = {
+    name:"Sid",
+    address:"Satara",
+    phone:9383929291,
+    isActive:true,
+}
+
+console.log(student);
+console.log(student.isActive);
+
