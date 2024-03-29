68e93e1f75827c8b1012cf572676f0b184d87cb6
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-29 15:17:58 +0530

    Add files via upload

---
+++ Program7.ts
@@ -0,0 +1,30 @@
+//Function
+
+//Case 1
+
+function Display(){
+    let Name : string= "Siddhesh"
+    console.log("My name is Siddhesh");
+}
+
+Display()
+
+//Case 2
+
+function Add(No1,No2){
+    let Ans=No1+No2;
+    console.log(Ans);
+}
+
+Add(10,11);
+
+//Case 3
+
+function Mult(No1:number,No2:number){
+    let Ans=No1*No2;
+    return Ans;
+}
+
+let finalAns = Mult(10,11);
+console.log(finalAns);
+
