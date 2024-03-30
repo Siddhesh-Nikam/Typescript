9e18e6429f24a47fe2b4457112b823ff74246905
Siddhesh Nikam <98577535+Siddhesh-Nikam@users.noreply.github.com>
2024-03-30 14:25:12 +0530

    Add files via upload

---
+++ Program10.ts
@@ -0,0 +1,15 @@
+//Tuple ->
+//Number of elements are fixed
+//Types of element are known and same
+
+let skill : [string,number] = ["programming",400];
+console.log(skill);
+
+console.log(skill[0]);
+
+console.log(skill[1]);
+
+
+
+
+
